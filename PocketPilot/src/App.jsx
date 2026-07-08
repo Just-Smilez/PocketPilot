d2ccde8ce53ad5a2a51abd6d8cda48226ff85573
Sanusi Kamaldeen <kamaldeenkorede05@gmail.com>
2026-07-08 08:42:58 +0100

    Initial React project setup

---
+++ PocketPilot/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <h1>PocketPilot 💰</h1>
+      <p>Take control of your finances.</p>
+
+          <div>
+      <h1>Sanusi Kamaldeen</h1>
+    <p>7/8/2026</p>
+    <button>Get Started</button>
+    </div>
+    </div>
+
+    
+  );
+}
+
+export default App;
